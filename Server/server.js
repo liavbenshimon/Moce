1a9ed71f8d31cfbb94dc2cecd09a519855bfd50a
liav <liav1232111@gmail.com>
2024-12-02 16:25:01 +0200

    all crud

--- Server/server.js
+++ Server/server.js
@@ -16,10 +16,13 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI)  // השתמש במיתר החיבור מקובץ ה-.env
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch(err => {
+    console.error("MongoDB connection error:", err);
+  });
 
 // Routes setup
 app.use('/users', userRoutes);
